a9041db49d5a3b18121b705c4a2dd33aab228c6a
dev-miaroman <arif.romanmia132@gmail.com>
2026-03-09 06:31:32 +0600

    adding the javascript full-fill for the  singin design

---
+++ js/signin.js
@@ -0,0 +1,16 @@
+const validUserName = "admin";
+const validPassword = "admin123";
+const userNameInput = document.getElementById("input-userName");
+const passwordInput = document.getElementById("input-password");
+
+document.getElementById("sign-in-button").addEventListener("click", function () {
+  if (userNameInput.value === validUserName && passwordInput.value === validPassword) {
+    window.location.href = "all.html";
+  } 
+  else {
+    userNameInput.value = "";
+    passwordInput.value = "";
+    document.getElementById("error-message").textContent = "Please try again.";
+    document.getElementById("signInModal").showModal();
+  }
+});
